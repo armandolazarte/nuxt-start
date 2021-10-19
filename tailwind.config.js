0a13d31d12836a8cddf0f98f57b9ff55c7f90db3
landing-russia <landing-russia@yandex.ru>
2021-10-19 10:53:52 +0300

    add tailwindss plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,5 +26,7 @@ module.exports = {
   plugins: [
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/line-clamp'),
   ]
 };
